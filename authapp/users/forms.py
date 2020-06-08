dceb80ca3cb5fb024a0b3f564b139e63bd561b6f
abcgco <abcgco@gmail.com>
2020-06-08 16:32:24 +0700

    two

--- authapp/users/forms.py
+++ authapp/users/forms.py
@@ -32,18 +32,34 @@ class UserCreationForm(forms.UserCreationForm):
         raise ValidationError(self.error_messages["duplicate_username"])
 
 
-from allauth.account.forms import SignupForm
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, HTML, Field, Div
+from crispy_forms.bootstrap import StrictButton
+from allauth.account.forms import SignupForm   
+
+
 class MyCustomSignupForm(SignupForm):
+
     first_name = CharField(max_length=30, label='First Name')
-    last_name = CharField(max_length=30, label='First Name')
+    last_name = CharField(max_length=30, label='Last Name')
+    # email = CharField(max_length=254, label='Email')
 
-    def save(self, request):
-
-        # Ensure you call the parent class's save.
-        # .save() returns a User object.
+    def save(self, request, user):
         user = super(MyCustomSignupForm, self).save(request)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
-        user.username = self.cleaned_data['email']
-        user.save()
         return user
+
+    def __init__(self, *args, **kwargs):
+        super(MyCustomSignupForm, self).__init__(*args, **kwargs)
+
+        self.helper = helper = FormHelper()
+        helper.html5_required = True
+
+        helper.layout = Layout(
+            Field('first_name'),
+            Field('last_name'),
+            Field('email'),
+            StrictButton('Отправить', css_class='btn btn-primary', type='submit'),
+        )         
